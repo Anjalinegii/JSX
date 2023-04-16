6f2ef6825066017651ce8d4fb551c89b3e9d8554
Anjalinegii <130498695+Anjalinegii@users.noreply.github.com>
2023-04-16 13:32:48 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+ReactDOM.render(
+  <div>
+    <h1>Hello</h1>
+    <p>I'm Anjali. </p>
+  </div>,
+  document.getElementById("root")
+);
